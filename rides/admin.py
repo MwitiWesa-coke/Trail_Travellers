d4b32bbf43928a441bc23ec3718d960d21fda269
MwitiWesa-coke <mwitiwesa4@gmail.com>
2025-08-06 16:00:43 +0300

    Registered rides in admin rides/admin

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Vehicle, Journey, Booking
 
-# Register your models here.
+#Vehicle admin
+@admin.register(Vehicle)
+class VehicleAdmin(admin,ModelAdmin):
+    list_display = ('vehicle_type', 'registration_numbrt', 'owner')
+    search_fields = ('registration_number', 'owner__full_name')
+
+#Journey admin
+@admin.register(Journey)
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'destination', 'driver', 'vehicle', 'price', 'depature_time')
+    list_filter = ('origin', 'destination', 'depature_time')
+    search_fields = ('origin', 'destination', 'driver__full_name')
+
+#Booking Admin
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('client', 'journey', 'booking_time', 'confirmed')
+    list_filter = ('confirmed',)
+    search_fields = ('client__full_name', 'journet__origin', 'journey__destination')
