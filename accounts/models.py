3acf9652b9cd1fbb781459341cce8565add657d8
MwitiWesa-coke <mwitiwesa4@gmail.com>
2025-08-06 09:06:31 +0300

    Updated Driver Model to take vehicles and ratings

--- accounts/models.py
+++ accounts/models.py
@@ -61,7 +61,10 @@ class Driver(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='driver_profile')
     license_number = models.CharField(max_length=50)
     national_id  = models.CharField(max_length=20)
-    
+    vehicle_type = models.CharField(max_length=50, default='Car')
+    rating = models.FloatField(default=5.0)
+    is_available = models.BooleanField(default=True)
+    experience_years = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"Driver: {self.user.full_name}"
