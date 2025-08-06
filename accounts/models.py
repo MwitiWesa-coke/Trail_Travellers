64c8263c7865a6e66ec2bb7bff8adcfddd9b80c4
MwitiWesa-coke <mwitiwesa4@gmail.com>
2025-08-06 08:58:26 +0300

    Created users, driver and Admins from shell and verified their roles

--- accounts/models.py
+++ accounts/models.py
@@ -61,6 +61,7 @@ class Driver(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='driver_profile')
     license_number = models.CharField(max_length=50)
     national_id  = models.CharField(max_length=20)
+    
 
     def __str__(self):
         return f"Driver: {self.user.full_name}"
